ff6e61754d50ad359c12495cc16583ad2bfc2546
Chris <chirssel@gmail.com>
2022-12-12 22:19:17 +0100

    Add support for non-alphabetic emotes

--- src/main/java/me/chrrrs/pegsemotes/mixin/ChatMessagesMixin.java
+++ src/main/java/me/chrrrs/pegsemotes/mixin/ChatMessagesMixin.java
@@ -12,7 +12,21 @@ public class ChatMessagesMixin {
     @ModifyVariable(method = "getRenderedChatMessage", at = @At("HEAD"), ordinal = 0, argsOnly = true)
     private static String getRenderedChatMessageParam(String message) {
         for (Emote emote : EmoteRegistry.getInstance().getEmotes()) {
-            message = message.replaceAll("\\b" + emote.getName() + "\\b", "\u00a8" + emote.getId());
+            String regex = emote.getName();
+
+            if (Character.isAlphabetic(regex.charAt(0))) {
+                regex = "\\b" + regex;
+            } else {
+                regex = "\\B" + regex;
+            }
+
+            if (Character.isAlphabetic(regex.charAt(regex.length() - 1))) {
+                regex = regex + "\\b";
+            } else {
+                regex = regex + "\\B";
+            }
+
+            message = message.replaceAll(regex, "\u00a8" + emote.getId());
         }
 
         return message;
