b5fe99e655993eb54d752fa441a5e713833dd90d
Chris <chirssel@gmail.com>
2022-12-14 12:24:06 +0100

    Reload emotes when joining a game

---
+++ src/main/java/me/chrrrs/pegsemotes/mixin/ClientPlayNetworkHandlerMixin.java
@@ -0,0 +1,17 @@
+package me.chrrrs.pegsemotes.mixin;
+
+import me.chrrrs.pegsemotes.emotes.EmoteRegistry;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.network.packet.s2c.play.GameJoinS2CPacket;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientPlayNetworkHandler.class)
+public class ClientPlayNetworkHandlerMixin {
+    @Inject(method = "onGameJoin", at = @At("RETURN"))
+    private void onGameJoin(GameJoinS2CPacket _packet, CallbackInfo _ci) {
+        new Thread(() -> EmoteRegistry.getInstance().reload(), "Emote Reloader").start();
+    }
+}
