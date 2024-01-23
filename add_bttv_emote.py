14ad7a096cc6ea9a41faa1dfcd9af2063f586258
Chris <git@chrr.me>
2024-01-23 17:31:40 +0100

    Improve BTTV script to output when operation is successful

--- add_bttv_emote.py
+++ add_bttv_emote.py
@@ -14,12 +14,13 @@ if "emotes" not in emotes:
     emotes["emotes"] = []
 
 for id in sys.argv[1:]:
-    res = requests.get("https://api.betterttv.net/3/emotes/" + id).json()
+    res = requests.get(f"https://api.betterttv.net/3/emotes/{id}").json()
 
     if "message" in res:
         print(f"could not add {id}: " + res["message"])
+        continue
 
-    url = "https://cdn.betterttv.net/emote/{}/2x.{}".format(res["id"], res["imageType"])
+    url = f"https://cdn.betterttv.net/emote/{res['id']}/2x.{res['imageType']}"
     image_res = requests.get(url)
     sha1 = hashlib.sha1(image_res.content).digest().hex()
 
@@ -29,6 +30,8 @@ for id in sys.argv[1:]:
         "url": url,
         "sha1": sha1,
     })
+
+    print(f"added {res['code']} ({id}, {res['imageType']})")
 
 emotes["emotes"].sort(key=lambda emote: emote["name"].lower())
 
