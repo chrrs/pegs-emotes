d27a45c858c0758005b0d49822dc6e01cb425c70
Chris <git@chrr.me>
2024-01-31 14:51:13 +0100

    Add reload keybind

--- src/main/java/me/chrr/pegsemotes/EmoteMod.java
+++ src/main/java/me/chrr/pegsemotes/EmoteMod.java
@@ -2,14 +2,22 @@ package me.chrr.pegsemotes;
 
 import me.chrr.pegsemotes.emote.RepositoryManager;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
 import net.minecraft.resource.ResourceManager;
 import net.minecraft.resource.ResourceType;
+import net.minecraft.text.Style;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.lwjgl.glfw.GLFW;
 
 import java.io.IOException;
 import java.nio.file.Path;
@@ -34,6 +42,7 @@ public class EmoteMod implements ClientModInitializer {
 
         RepositoryManager.getInstance().setRepositories(config.repositories);
 
+        // Reload emotes when client resources get reloaded
         ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES)
                 .registerReloadListener(new SimpleSynchronousResourceReloadListener() {
                     @Override
@@ -47,5 +56,19 @@ public class EmoteMod implements ClientModInitializer {
                     }
                 });
 
+        // Reload emotes when key bind is pressed (F4 by default)
+        KeyBinding reloadKeyBinding = KeyBindingHelper.registerKeyBinding(
+                new KeyBinding("key.pegs-emotes.reload", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_F4, "category.pegs-emotes"));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (reloadKeyBinding.wasPressed()) {
+                RepositoryManager.getInstance().reload();
+
+                if (client.player != null) {
+                    client.player.sendMessage(Text.translatable("text.pegs-emotes.reload-complete", RepositoryManager.getInstance().getEmoteNames().size())
+                            .setStyle(Style.EMPTY.withFormatting(Formatting.GRAY)), false);
+                }
+            }
+        });
     }
 }
